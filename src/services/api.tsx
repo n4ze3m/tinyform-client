844d76692bd0bf1a838c4d43e00074502742985a
nazeem <nazeemnob18@gmail.com>
2022-04-24 19:48:20 +0530

    new changes

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { getAccessToken, getRefreshToken, removeTokens, setTokens } from "./token";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
