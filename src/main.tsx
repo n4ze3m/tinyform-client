8835533b96e1a0eb3173d3cc43600f8a6fb37b53
nazeem <nazeemnob18@gmail.com>
2022-04-30 23:23:07 +0530

    font imported to global

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       withNormalizeCSS
       theme={{
         colorScheme: 'dark',
+        fontFamily: 'Montserrat',
       }}
     >
       <NotificationsProvider>
